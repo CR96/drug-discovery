22c58346924b89a43e75d486d510f3eb35f44aa6
Corey Rowe <coreyr1196@gmail.com>
2019-06-20 08:46:31 -0400

    Create subdirectories if needed

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 # Run the docking simulation.
 # TODO: Set up a parallel processing batch script that screens all compounds in ligand/ against a specified enzyme in protein/
 
+# Create output and log directories if they don't exist
+mkdir -p log/
+mkdir -p output/
+
 vina --config config/2ate_conf.txt --log log/2ate_log.txt
