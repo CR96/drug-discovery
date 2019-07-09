2e6645678465de05ea30d209e981ba62b5a0d9d6
Corey Rowe <coreyr1196@gmail.com>
2019-07-08 20:03:18 -0400

    Add author attribution to run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 # Run the docking simulation.
+
+# This script was adapted from a script written by the authors of [AutoDock Vina.](https://vina.scripps.edu) i
+# The original is available at https://vina.scripps.edu/vina_screen_local.sh.
+
+# Trott O, Olson AJ.
+# AutoDock Vina: improving the speed and accuracy of docking
+# with a new scoring function, efficient optimization and
+# multithreading. J. Comput. Chem. 2010;31(2):455-461.
+# doi:10.0112/jcc.21334
+
 # TODO: Set up a parallel processing batch script that screens all compounds in ligand/ against a specified enzyme in protein/
 
 # Create output and log directories if they don't exist
