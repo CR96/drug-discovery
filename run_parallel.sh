184da58e495b1c6eee9c0be39279409bab3ba168
Corey Rowe <coreyr1196@gmail.com>
2019-07-27 02:23:56 -0400

    Fix array index
    $PBS_ARRAY_INDEX starts at 1, so it does not need to be offset

--- run_parallel.sh
+++ run_parallel.sh
@@ -21,8 +21,8 @@
 #PBS -r y
 
 # Process ligand files by directory
-for f in ligand/$(($PBS_ARRAY_INDEX + 1))/*.pdbqt; do
+for f in ligand/$PBS_ARRAY_INDEX/*.pdbqt; do
     b=$(basename $f .pdbqt)
-    echo Processing ligand $b in subdirectory $(($PBS_ARRAY_INDEX + 1))
+    echo Processing ligand $b in subdirectory $PBS_ARRAY_INDEX
     vina --config config/2ate_conf.txt --ligand $f --out output/${b}_docked.pdbqt
 done
