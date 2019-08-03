f459242cde11b026e709498e447e8a611c122d10
Corey Rowe <coreyr1196@gmail.com>
2019-08-03 09:01:01 -0400

    Update zinc_download.sh to read from static mirror at files.docking.org

--- zinc_download.sh
+++ zinc_download.sh
@@ -1,4 +1,4 @@
-# Download ligand files from the ZINC15 database.
+# Download ligand files from the static ZINC15 mirror at https://files.docking.org
 
 # Sterling T, Irwin JJ.
 # ZINC 15 — Ligand discovery for everyone.
@@ -6,23 +6,54 @@
 # doi:10.1021/acs.jcim.5b00559
 
 #!/bin/bash
-#PBS -N rowec_ligand_download
-#PBS -l select=1:ncpus=8:mem=8gb
-#PBS -q wsuq
+
+# Note: This is an interactive script. It is not designed to be run unattended.
 
 cd ligand/
 
-read -p "Enter ZINC15 catalog name as it appears in the catalog URL (e.g. dbfda): " catalog_name
+echo "30, 40, 50: In stock for immediate delivery"
+echo "20: Make on demand"
+echo "10: Boutique"
+echo "7: One-Step"
+echo "3: May be available plated in libraries"
+echo "1: Not for sale"
+read -p "Select a catalog directory by number: " catalog_directory
 
-echo Downloading...
+echo "Entering command-line web browser."
+echo "Note the directory name of the catalog you wish to download, then press Q twice to exit the browser."
+read -p "Press Enter to continue "
 
-### Retrieve all substances in requested catalog
-curl https://zinc15.docking.org/catalogs/$catalog_name/substances.txt -F output_fields="zinc_id" -F count=all > zinc.txt
+lynx https://files.docking.org/catalogs/$catalog_directory/
 
-split -l 100 zinc.txt
+read -p "Enter the name of the catalog to download: " catalog_name
 
-for i in x??; do
-	curl https://zinc15.docking.org/protomers/subsets/usual.mol2.gz -F count=all F zinc_id-in=@$i > $i.mol2.gz
-done
+echo "Downloading PDBQT tranche lists..."
+echo "=============="
+wget https://files.docking.org/catalogs/$catalog_directory/$catalog_name/sdi-pdbqt-frags
+wget https://files.docking.org/catalogs/$catalog_directory/$catalog_name/sdi-pdbqt-leads
+wget https://files.docking.org/catalogs/$catalog_directory/$catalog_name/sdi-pdbqt-lugs
 
-echo Download complete.
+sed -i '1s/^/# PDBQT Fragments\n/' sdi-pdbqt-frags
+sed -i '1s/^/# PDBQT Leads\n/' sdi-pdbqt-leads
+sed -i '1s/^/# PDBQT Lugs\n/' sdi-pdbqt-lugs
+
+cat sdi-pdbqt-frags sdi-pdbqt-leads sdi-pdbqt-lugs > download
+
+echo "Done. Now review the list and remove any tranches you do not wish to download."
+read -p "Press Enter to continue "
+vim download
+
+echo "Generating download script..."
+
+# The resulting downloaded file is a list of file URLs.
+# Add 'wget' to each line and make the file executable.
+sed -i -e 's/^/wget /' download
+chmod a+x download
+
+echo "Done."
+read -p "Press Enter to begin the download. This may take a long time. "
+echo "Downloading..."
+echo "=============="
+./download
+rm ./download
+echo "Download complete."
