96cc360cfd9fa8fbc9e79f24b67d6e0375b2d45e
Corey Rowe <coreyr1196@gmail.com>
2019-10-21 18:15:19 -0400

    Update vina_screen_get_top.py
    Repurpose this script to list the ZINC ID and binding energy of every
    file in an AutoDock Vina results directory, rather than a subset.
    The output can be sorted and filtered in a spreadsheet program later.

--- vina_screen_get_top.py
+++ vina_screen_get_top.py
@@ -1,8 +1,9 @@
 #! /usr/bin/python2.7
-# Find the lowest binding energies within a set of AutoDock output files.
 
-# This script was written by the authors of [AutoDock Vina.](https://vina.scripps.edu)
-# It is available at https://vina.scripps.edu/vina_screen_get_top.py
+# Print the filename, ZINC ID, and binding energy for every file within a directory of AutoDock Vina results.
+
+# This is a modified version of a script written by the authors of [AutoDock Vina.](https://vina.scripps.edu)
+# The original is available at https://vina.scripps.edu/vina_screen_get_top.py
 
 # Trott O, Olson AJ.
 # AutoDock Vina: improving the speed and accuracy of docking
@@ -10,31 +11,30 @@
 # multithreading. J. Comput. Chem. 2010;31(2):455-461.
 # doi:10.0112/jcc.21334
 
+import os
 import sys
 import glob
 
-def doit(n):
-    file_names = glob.glob('results/*.pdbqt')
+
+def doit():
+    os.chdir("./results")
+    file_names = glob.glob('*.pdbqt')
     everything = []
     failures = []
-    print 'Found', len(file_names), 'pdbqt files'
     for file_name in file_names:
-        file = open(file_name)
-        lines = file.readlines()
-        file.close()
+        result_file = open(file_name)
+        lines = result_file.readlines()
+        result_file.close()
         try:
-            line = lines[1]
-            result = float(line.split(':')[1].split()[0])
-            everything.append([result, file_name])
+            result = float(lines[1].strip().split(':')[1].split()[0])
+            zinc = lines[2].strip().split('=')[1]
+            everything.append([file_name, zinc, result])
         except:
             failures.append(file_name)
-    everything.sort(lambda x,y: cmp(x[0], y[0]))
-    part = everything[:n]
-    for p in part:
-        print p[1],
-    print
-    if len(failures) > 0:
-        print 'WARNING:', len(failures), 'pdbqt files could not be processed'
+    everything.sort(lambda x, y: cmp(x[0], y[0]))
+    for i in everything:
+        print i[0], "\t", i[1], "\t", i[2]
+
 
 if __name__ == '__main__':
-    doit(int(sys.argv[1]))
+    doit()
