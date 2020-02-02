f20c173e5d8eebf88f8a1fa2ec00f11ee0fa6af6
Corey Rowe <coreyr1196@gmail.com>
2020-02-02 18:23:24 -0500

    Add split_sdf.py
    Arguments: Base filename, number of ligands per split file

---
+++ split_sdf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+# This script downloaded from Pankaj Mishra on ResearchGate.
+# https://www.researchgate.net/profile/Pankaj_Mishra5
+
+# Two arguments: Base filename, number of ligands per split file
+
+import sys
+f= sys.argv[1]
+split_number = int(sys.argv[2])
+number_of_sdfs = split_number
+i=0
+j=_0
+f2=open(f+'_'+str(j)+'.sdf','w')
+for line in open(f+'.sdf'):
+	f2.write(line)
+	if line[:4] == "$$$$":
+		i+=1
+	if i > number_of_sdfs:
+		number_of_sdfs += split_number
+		f2.close()
+		j+=1
+		f2=open(f+'_'+str(j)+'.sdf','w')
+print(i)
+
