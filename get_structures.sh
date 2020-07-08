123f9c7ac456a655d9c2b021f8ea4bb21904606a
Corey Rowe <coreyr1196@gmail.com>
2020-07-08 00:02:39 -0400

    Add get_structures.sh
    Extracts the chemical structure name from each docked ligand
    and saves it to a text file

---
+++ get_structures.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# Create a list of the chemical structure of each ligand docked.
+
+# Copy stdout and stderr into a single log ile
+exec > >(tee -i log/2ate_structure_list.txt)
+exec 2>&1
+for f in results/*.pdbqt; do
+	echo "Ligand name: $f"
+	grep 'REMARK  Name = ' $f | sed -i -e 's/"REMARK  Name = "//g'
+done
