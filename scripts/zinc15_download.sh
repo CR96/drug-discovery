c54e17e15812b99b887af5f5c40fcee1d09f75a1
Corey Rowe <coreyr1196@gmail.com>
2021-10-29 14:14:26 -0400

    Replace zinc15 references with zinc20 in zinc15_download.sh

--- scripts/zinc15_download.sh
+++ scripts/zinc15_download.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Download ligand files from ZINC15 or the static ZINC15 mirror at https://files.docking.org
+# Download ligand files from ZINC20 or the static ZINC mirror at https://files.docking.org
 
-# Sterling T, Irwin JJ.
-# ZINC 15 — Ligand discovery for everyone.
-# J. Chem. Inf. Model. 2015;55:2324-2337.J. Comput. Chem. 2010;31(2):455-461.
-# doi:10.1021/acs.jcim.5b00559
+# Irwin JJ, Tang KG, Young J, et al.
+# ZINC20 — A free ultralarge-scale chemical database for ligand discovery.
+# J. Chem. Inf. Model. 2020, 60, 6065-6073.
+# doi:10.1021/acs.jcim.0c00675
 
 cd ../ligands/
 
 read -p "Enter the name of the catalog to download: " catalog_name
 
-curl  https://zinc15.docking.org/catalogs/$catalog_name/substances.txt -F output_fields="zinc_id" -F count=all > zinc.txt
+curl  https://zinc20.docking.org/catalogs/$catalog_name/substances.txt -F output_fields="zinc_id" -F count=all > zinc.txt
 
 ## Sort ZINC ID entries numerically and remove any duplicates
 sort zinc.txt | uniq > zinc_sorted.txt
@@ -21,7 +21,7 @@ for f in x??.txt; do
 	[ -f "$f" ] || break
 	b=$(basename $f .txt)
 	output=${catalog_name}_${b}
-	curl https://zinc15.docking.org/protomers/subsets/usual.mol2.gz -F count=all -F zinc_id-in=@$f > $output.mol2
+	curl https://zinc20.docking.org/protomers/subsets/usual.mol2.gz -F count=all -F zinc_id-in=@$f > $output.mol2
 done
 
 rm x*.txt
